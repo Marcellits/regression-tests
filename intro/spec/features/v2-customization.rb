a0c3132d44faa4c989b0f3e5e47251f6ad17216a
marcellits <garcia.marcella@hotmail.com>
2021-07-02 18:27:58 -0400

    spec when aff has logo image added

--- intro/spec/features/v2-customization.rb
+++ intro/spec/features/v2-customization.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require_relative '../steps.rb'
 require_relative '../pws.rb'
+require 'dotenv/load'
 
 
 feature "Affiliation's Customization" do
@@ -11,9 +12,7 @@ feature "Affiliation's Customization" do
         click_on('Search')
         click_on('Actions')
         click_link('Org Params')
-        tr = page.find(:xpath, '//*[@id="org-param-65139"]') # get the parent tr of the td
-        primary_color = '#0099aa'
-        expect(tr).to have_css('td', text: primary_color )
+        primary_color = page.find(:xpath, '//*[@id="org-param-65139"]/td[2]').text
         visit('/sign-in')
         conehealth_user_login
         background_color = page.find(:xpath, '/html/body/div[4]').native.css_value('background')
@@ -24,9 +23,16 @@ feature "Affiliation's Customization" do
     end
 
     scenario 'When Affiliation has logo added' do
-       
+        visit('https://stage-af.mdlive.com/affiliation_configurators/sign_in')
+        aff_manager_admin_login
+        fill_in('affiliation-search-field', with: 'conehealth')
+        click_on('Search')
+        click_on('Actions')
+        click_link('Affiliation Images')
+        image_link = page.find(:xpath, '//*[@id="affiliation-image-2286"]/td[2]').text
+        visit('/sign-in')
+        conehealth_user_login
+        logo_port = page.find(:xpath, '//*[@id="affiliationLogo"]')['src']
+        expect(logo_port).to eq(image_link)
     end
-
-
-
 end    
