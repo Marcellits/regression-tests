1c89f99b4d46bd5d4987a72dd48bcb3ff370afa4
marcellits <garcia.marcella@hotmail.com>
2021-07-08 18:56:35 -0400

    spec for 270 user registration on landing pages

--- intro/spec/steps.rb
+++ intro/spec/steps.rb
@@ -33,3 +33,21 @@ def eligible_members_endpoint
     })
     p JSON.parse(response)
 end
+
+def hash_month_names(month)
+    hash_months = {
+        '01'=> 'January',
+        '02'=>' February',
+        '03'=> 'March',
+        '04'=> 'April',
+        '05'=> 'May',
+        '06'=> 'June',
+        '07'=> 'July',
+        '08'=> 'August',
+        '09'=> 'September',
+        '10'=> 'October',
+        '11'=> 'November',
+        '12'=> 'December',
+    }
+    hash_months[month]
+end
